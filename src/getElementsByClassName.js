58586b161e3a730be4a899cb2566f030af5b7e35
elizabethcan <elizabeth230@gmail.com>
2019-01-03 10:31:28 -0800

    finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,50 +5,17 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className, element) {
-  //document.body: is the element that contains the content for the document. In documents with <body> contents, returns the <body> element, and in frameset documents, this returns the outermost <frameset> element.
-  //element.childNodes: read-only property returns a live NodeList of child nodes of the given element where the first child node is assigned index 0.
-  //element.children: returns a collection of an element's child elements, as an HTMLCollection object. ... The difference between this property and childNodes, is that childNodes contain all nodes, including text nodes and comment nodes, while children only contain element nodes.
-  //element.classList: is a read-only property that returns a live DOMTokenList collection of the class attributes of the element.
-  
-  //input: string output: an array of all elements whose class contains className
-  //check document body for elements with className
-  //check each element for children elements with className
-  //repeat
-  
   var result = [];
-  
-  var body = document.body;
-  
-  if (body.classList.contains(className)) {
-    result.push(body);
+  element = element || document.body;
+
+  if (element.classList.contains(className)) {
+    result.push(element);
   }
   
-  currentElement = body.children; //html coll
-    var array = Array.prototype.slice.call( currentElement )
-    
-    if (array.length > 0) {
-      array.forEach(function(el) {
-        if (el.classList.contains(className) {
-          result.push(el);
-        });
-      });
-    }
-  // if (currentElement.length > 0) {
-  //   for(var i = 0; i < currentElement.length; i++) {
-  //     if (currentElement[i].classList.contains(className)) {
-  //       result.push(currentElement[i]);
-  //       currentElement = currentElement[i].children
-  //       if (currentElement.length > 0) {
-  //         for(var j = 0; j < currentElement.length; j++) {
-  //           if (currentElement[j].classList.contains(className)) {
-  //             result.push(currentElement[j]);
-  //           }
-  //         }
-  //       }
-  //     }
-      //getElementsByClassName(className, currentElement[i]);
-  //   }
-  // }
+  var currentElement = element.children;
+  for(var i = 0; i < currentElement.length; i++) {
+    result = result.concat(getElementsByClassName(className, currentElement[i]));
+  }
   
   return result;
 };
